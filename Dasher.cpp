0b485a04a4ad943ecf310f3686cbc002da8f6241
Julia Jomon <juliajomon12@gmail.com>
2024-06-28 13:49:43 +0530

    added enemy, edited animation

--- Dasher.cpp
+++ Dasher.cpp
@@ -25,25 +25,41 @@ int main(){
 
     //sprite sheet
     Texture2D scrappy = LoadTexture("dapper-dasher-master-textures/textures/scarfy.png");
+    Texture2D fire = LoadTexture("dapper-dasher-master-textures/textures/12_nebula_spritesheet.png");
+
+    //fire dimension
+    Rectangle fire_size{0.0,0.0, fire.width/8,fire.height/8};
+
+    //fire postion
+    Vector2 fire_pos{width,length-fire_size.height};
+    
     //character dimension
     Rectangle scrappy_size ;
     scrappy_size.width = scrappy.width/6;
     scrappy_size.height = scrappy.height;
     scrappy_size.x=0;
     scrappy_size.y=0;
+    
     //character position
     Vector2 scrappy_pos ;
     scrappy_pos.x = width/2 - scrappy_size.width/2;
     scrappy_pos.y = length - scrappy_size.height;
 
+    //fire velocity pixel/sec
+    int fire_vel{-500};
 
-    
+    //animation frame 
+    int frame{};
+    float UpdateTime = 1.0/12.0 ;
+    float runningtime{};
 
     while(WindowShouldClose() == false){
 
         if(IsKeyDown(KEY_F11)){
             ToggleFullscreen();
         }
+
+
 
         const float dt = GetFrameTime(); //delta time
 
@@ -65,14 +81,29 @@ int main(){
             velocity += jump;
         }
         
-       
-        scrappy_pos.y += velocity * dt;
+       fire_pos.x += fire_vel *dt;
+        scrappy_pos.y += velocity * dt; //update scrappy position
 
-        DrawTextureRec(scrappy,scrappy_size,scrappy_pos, WHITE);
+        //animation
+        runningtime += dt ;
+        if(isInair == false){
+            if(runningtime >= UpdateTime)
+            {
+                runningtime = 0.0; //timer restart
+                scrappy_size.x = frame * scrappy_size.width;
+                frame++;
+                if(frame > 5){
+                    frame = 0;
+                }
+            }
+        }
+        DrawTextureRec(fire,fire_size,fire_pos,WHITE);//drwaing fire
+        DrawTextureRec(scrappy,scrappy_size,scrappy_pos, WHITE); //drawing character
         
         EndDrawing();
 
     }
     UnloadTexture(scrappy);
+    UnloadTexture(fire);
     CloseWindow();
 }
